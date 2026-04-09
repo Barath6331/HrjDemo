0a5c55031b8532392dac04e06143094b9b2e31c1
BARATH <barathkumar63331@gmail.com>
2026-04-09 18:46:37 +0530

    login inprogress completed

--- app/src/main/java/com/example/hrjdemo/ui/theme/utils/Navigation.kt
+++ app/src/main/java/com/example/hrjdemo/ui/theme/utils/Navigation.kt
@@ -4,6 +4,7 @@ sealed class Navigation(val route: String) {
 
     object Splash : Navigation("splash")
     object AccountSelection : Navigation("accountselection")
+    object LoginRegisterSelection: Navigation("loginregisterselection")
     object Login : Navigation("login")
     object Dashboard : Navigation("dashbaord")
 }
---
+++ app/src/main/java/com/example/hrjdemo/ui/theme/utils/OtpView.kt
@@ -0,0 +1,68 @@
+package com.example.hrjdemo.ui.theme.utils
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun OtpView(
+    otpLength: Int = 6,
+    onOtpChange: (String) -> Unit
+) {
+    var otp by remember { mutableStateOf("") }
+
+    Box(contentAlignment = Alignment.Center) {
+
+        // Hidden TextField
+        BasicTextField(
+            value = otp,
+            onValueChange = {
+                if (it.length <= otpLength) {
+                    otp = it.filter { ch -> ch.isDigit() }
+                    onOtpChange(otp)
+                }
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier
+                .matchParentSize()
+                .alpha(0f) // invisible
+        )
+
+        // Visible boxes
+        Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+            repeat(otpLength) { index ->
+                val char = otp.getOrNull(index)?.toString() ?: ""
+
+                Box(
+                    modifier = Modifier
+                        .size(50.dp)
+                        .border(1.dp, Color.Gray, RoundedCornerShape(8.dp)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = char,
+                        fontSize = 20.sp
+                    )
+                }
+            }
+        }
+    }
+}
