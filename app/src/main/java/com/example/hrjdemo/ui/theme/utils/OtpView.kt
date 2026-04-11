199caa4ff87309e29738294b10a7721716a58560
BARATH <barathkumar63331@gmail.com>
2026-04-11 18:38:39 +0530

    Login page inprogress

--- app/src/main/java/com/example/hrjdemo/ui/theme/utils/OtpView.kt
+++ app/src/main/java/com/example/hrjdemo/ui/theme/utils/OtpView.kt
@@ -1,6 +1,7 @@
 package com.example.hrjdemo.ui.theme.utils
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -10,6 +11,7 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -17,6 +19,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -28,10 +32,19 @@ fun OtpView(
     onOtpChange: (String) -> Unit
 ) {
     var otp by remember { mutableStateOf("") }
+    val focusRequester = remember { FocusRequester() }
 
-    Box(contentAlignment = Alignment.Center) {
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
 
-        // Hidden TextField
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.clickable {
+            focusRequester.requestFocus()
+        }
+    ) {
+
         BasicTextField(
             value = otp,
             onValueChange = {
@@ -43,24 +56,26 @@ fun OtpView(
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             modifier = Modifier
                 .matchParentSize()
-                .alpha(0f) // invisible
+                .alpha(0f)
+                .focusRequester(focusRequester)
         )
 
-        // Visible boxes
         Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
             repeat(otpLength) { index ->
                 val char = otp.getOrNull(index)?.toString() ?: ""
+                val isFocused = index == otp.length
 
                 Box(
                     modifier = Modifier
                         .size(50.dp)
-                        .border(1.dp, Color.Gray, RoundedCornerShape(8.dp)),
+                        .border(
+                            1.dp,
+                            if (isFocused) Color.Black else Color.Gray,
+                            RoundedCornerShape(8.dp)
+                        ),
                     contentAlignment = Alignment.Center
                 ) {
-                    Text(
-                        text = char,
-                        fontSize = 20.sp
-                    )
+                    Text(text = char, fontSize = 20.sp)
                 }
             }
         }
--- app/src/main/java/com/example/hrjdemo/ui/theme/view/loginRegister/login/Login.kt
+++ app/src/main/java/com/example/hrjdemo/ui/theme/view/loginRegister/login/Login.kt
@@ -1,15 +1,20 @@
 package com.example.hrjdemo.ui.theme.view.loginRegister.login
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
@@ -22,6 +27,8 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -31,7 +38,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -44,6 +52,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.hrjdemo.R
+import com.example.hrjdemo.ui.theme.utils.AppButton
 import com.example.hrjdemo.ui.theme.utils.OtpView
 
 
@@ -51,6 +60,7 @@ import com.example.hrjdemo.ui.theme.utils.OtpView
 @Composable
 fun Login(navController: NavController) {
 
+    val context = LocalContext.current
     Scaffold(
         topBar = {
             TopAppBar(
@@ -72,13 +82,15 @@ fun Login(navController: NavController) {
         },
         containerColor = colorResource(R.color.colorSecondary)
     ) { padding ->
-        Box() {
+        Box(modifier = Modifier.fillMaxSize().padding(padding)) {
             var mobileNumber by rememberSaveable { mutableStateOf("") }
+            var OtpFeildVisiblity by rememberSaveable { mutableStateOf(false) }
+            var isTextFieldEnabled by rememberSaveable { mutableStateOf(true) }
             Column() {
                 Column(
                     modifier = Modifier
-                        .padding(padding)
                         .background(color = colorResource(R.color.white))
+                        .wrapContentHeight()
                 ) {
                     Spacer(modifier = Modifier.height(20.dp))
 
@@ -119,51 +131,98 @@ fun Login(navController: NavController) {
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(45.dp)
-                        .padding(start = 20.dp, end = 20.dp)
+                        .padding(horizontal = 16.dp)
                         .background(
-                            shape = RoundedCornerShape(5.dp),
-                            color = colorResource(R.color.white)
-                        )
-                        .border(
-                            width = (1.dp),
-                            color = colorResource(R.color.grey),
+                            color = colorResource(R.color.white),
                             shape = RoundedCornerShape(5.dp)
                         )
+                        .border(
+                            1.dp,
+                            colorResource(R.color.grey),
+                            RoundedCornerShape(5.dp)
+                        ),
+                    contentAlignment = Alignment.CenterStart
                 ) {
+
+                    if (mobileNumber.isEmpty()) {
+                        Text(
+                            text = "Enter MobileNumber",
+                            color = Color.Gray,
+                            modifier = Modifier.padding(horizontal = 10.dp)
+                        )
+                    }
+
                     BasicTextField(
                         value = mobileNumber,
-                        onValueChange = { mobileNumber = it },
-                        modifier = Modifier.fillMaxWidth(),
+                        onValueChange = {
+                            if (isTextFieldEnabled) {
+                                mobileNumber = it.filter { c -> c.isDigit() }.take(10)
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 10.dp),
+                        singleLine = true,
                         keyboardOptions = KeyboardOptions(
                             keyboardType = KeyboardType.Number,
                             imeAction = ImeAction.Done
                         ),
                         keyboardActions = KeyboardActions(
                             onDone = {
-
+                                validate(mobileNumber,context){
+                                    OtpFeildVisiblity = true
+                                    isTextFieldEnabled = false
+                                }
                             }
+                        ),
+                        textStyle = TextStyle(
+                            fontFamily = FontFamily(Font(R.font.avenirnextltpro_regular)),
+                            color = colorResource(R.color.black),
+                            fontSize = 14.sp
                         )
                     )
                 }
+                if (OtpFeildVisiblity) {
+                    Box(
+                        modifier = Modifier
+                            .padding(top = 30.dp)
+                            .fillMaxWidth(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        OtpView { otp ->
+                            println("OTP entered: $otp")
 
-
-
-                Box(
-                    modifier = Modifier
-                        .padding(padding)
-                        .fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    OtpView { otp ->
-                        println("OTP entered: $otp")
-
-                        if (otp.length == 6) {
-                            // verify OTP
+                            if (otp.length == 6) {
+                                // verify OTP
+                            }
                         }
                     }
+                }
+            }
+
+            AppButton(
+                text = "Login",
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(top = 20.dp, bottom = 30.dp, start = 20.dp, end = 20.dp)
+            ) {
+                validate(mobileNumber,context){
+                    OtpFeildVisiblity = true
+                    isTextFieldEnabled = false
                 }
             }
         }
     }
 
+}
+fun validate(mobile : String, context: Context, onSuccess:()-> Unit){
+    when {
+        mobile.isEmpty() -> {
+            Toast.makeText(context, "Please Enter Mobile Number", Toast.LENGTH_LONG).show()
+        }
+        mobile.length != 10 -> {
+            Toast.makeText(context, "Please Enter Valid Mobile Number", Toast.LENGTH_LONG).show()
+        }
+        else -> onSuccess()
+    }
 }
